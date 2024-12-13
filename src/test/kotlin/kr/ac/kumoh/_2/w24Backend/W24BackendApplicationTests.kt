5e0fdce021d5c64c8912e3a0e8d75cbdaf597370
kyh <juuipason25@gmail.com>
2024-12-13 16:24:09 +0900

    🎉 Term Project Backend 시작

---
+++ src/test/kotlin/kr/ac/kumoh/_2/w24Backend/W24BackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package kr.ac.kumoh._2.w24Backend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class W24BackendApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
